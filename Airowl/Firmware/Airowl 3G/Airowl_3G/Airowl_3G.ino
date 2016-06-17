8f8197f69fe0678388e40b0e49525f10995bb2da
thesiddjain <jain.siddharth.1809@gmail.com>
2016-06-17 14:41:11 +0530

    correction

--- Airowl/Firmware/Airowl 3G/Airowl_3G/Airowl_3G.ino
+++ Airowl/Firmware/Airowl 3G/Airowl_3G/Airowl_3G.ino
@@ -29,7 +29,7 @@ void setup()
 {
   GSM_Serial.begin(9600);              // the GPRS baud rate   
   Dust_Serial.begin(9600);             // Dust Sensor baud rate
-  Serial.begin(19200);                 // the GPRS baud rate 
+  //Serial.begin(19200);                 // the GPRS baud rate 
   pinMode(A0, OUTPUT);
   pinMode(A1, OUTPUT);
   pinMode(A2, OUTPUT);
@@ -59,7 +59,7 @@ void loop()
    
   // GSM_Serial.listen();  
    SubmitHttpRequest();
-   delay(2000);
+   delay(5000);
    Serial.println("***********************************************************************************************");
 
   LED_blink(); // Blinking LED's of Airowl eyes.
