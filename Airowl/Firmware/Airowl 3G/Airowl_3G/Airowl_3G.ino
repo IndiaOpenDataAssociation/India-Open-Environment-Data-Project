adf82a2423e5d31e47d92ceb2c1ccf07bc621a62
IndiaOpenDataAssociation <namit.singh@jsm.email>
2016-06-28 15:23:00 +0530

    updated data sending timer

--- Airowl/Firmware/Airowl 3G/Airowl_3G/Airowl_3G.ino
+++ Airowl/Firmware/Airowl 3G/Airowl_3G/Airowl_3G.ino
@@ -3,16 +3,7 @@
 SoftwareSerial GSM_Serial(7, 8);
 SoftwareSerial Dust_Serial(2, 3);
 
-int PM1 = 0, PM25 = 0, PM10 = 0;
-int count = 0;
-
-String Data = "";
-String command = "";
 String deviceID = "AirOwl_";
-
-unsigned long lastPublishMillis;
-
-byte data[24];
 
 void setup()
 {
@@ -20,7 +11,7 @@ void setup()
 
   Dust_Serial.begin(9600);             // Dust Sensor baud rate
 
-  //Serial..begin(9600);                 // the GPRS baud rate
+  //Serial.begin(9600);                 // the GPRS baud rate
 
   //LED initiation
   analogWrite(A0, 1024); delay(1000); analogWrite(A0, 0);
@@ -29,6 +20,27 @@ void setup()
   delay(1000);
   blue();
 
+  init_winson();
+
+  deviceId();
+}
+
+void loop()
+{
+  unsigned int PM1, PM25, PM10;
+
+  if (Winsen_dust(PM1, PM25, PM10))
+  {
+    SubmitHttpRequest(PM1, PM25, PM10);
+    //Serial.println("***********************************************************************************************");
+    delay(1000);
+    LED_blink(PM1, PM25, PM10); // Blinking LED's of Airowl eyes.
+    delay(1000);
+  }
+}
+
+void init_winson()
+{
   const unsigned char cmd_get_sensor[] =
   {
     0xff, 0x01, 0x78, 0x40, 0x00,
@@ -40,59 +52,33 @@ void setup()
   {
     Dust_Serial.write(cmd_get_sensor[i]);
   }
-  delay(10);
-
-  deviceId();
-  lastPublishMillis = millis();
-
+  delay(1000);
 }
 
-void loop()
-{
-
-  Winsen_dust();
-  delay(2000);
-
-  if (millis() - lastPublishMillis > 20000UL)
-  {
-    SubmitHttpRequest();
-    //Serial..println("***********************************************************************************************");
-    delay(1000);
-    LED_blink(); // Blinking LED's of Airowl eyes.
-    PM1 = 0; PM25 = 0; PM10 = 0; count = 0;
-    lastPublishMillis = millis();
-    //delay(5000);
-  }
-}
-
-
-void SubmitHttpRequest()
+void SubmitHttpRequest(unsigned int PM1, unsigned int PM25, unsigned int PM10)
 {
   GSM_Serial.listen();
 
-  PM1 = int(PM1 / count);
-  PM25 = int(PM25 / count);
-  PM10 = int(PM10 / count);
-
-  GSM_Serial.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");//setting the SAPBR, the connection type is using gprs
+  rx_empty();
+  GSM_Serial.println("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\""); // setting the SAPBR, the connection type is using gprs
 
   if (check(0, 5000))
   {
-    //Serial..println("GPRS connection ok");
+    //Serial.println("GPRS connection ok");
   }
   else
   {
-    //Serial..println("GPRS connection err");
+    //Serial.println("GPRS connection err");
   }
 
-  //  GSM_Serial.println("AT+SAPBR=3,1,\"APN\",\"isafe\"");//setting the APN, the second need you fill in your local apn server
+  //  GSM_Serial.println("AT+SAPBR=3,1,\"APN\",\"isafe\""); // setting the APN, the second need you fill in your local apn server
   //  if(check(0, 5000))
   //  {
-  //    //Serial..println("APN connection ok");
+  //    //Serial.println("APN connection ok");
   //  }
   //  else
   //  {
-  //    //Serial..println("APN connection err");
+  //    //Serial.println("APN connection err");
   //  }
   //  delay(4000);
 
@@ -101,12 +87,12 @@ void SubmitHttpRequest()
     GSM_Serial.println("AT+SAPBR=1,1");//setting the APN, the second need you fill in your local apn server
     if (check(0, 10000))
     {
-      //Serial..println("SAPBR connection ok");
+      //Serial.println("SAPBR connection ok");
       break;
     }
     else
     {
-      //Serial..println("SAPBR connection err");
+      //Serial.println("SAPBR connection err");
     }
   }
 
@@ -116,17 +102,18 @@ void SubmitHttpRequest()
 
   if (check(0, 5000))
   {
-    //Serial..println("HTTP connection ok");
+    //Serial.println("HTTP connection ok");
   }
   else
   {
-    //Serial..println("HTTP connection err");
+    //Serial.println("HTTP connection err");
   }
 
   rx_empty();
-  command = "AT+HTTPPARA=\"URL\",\"http://oedpdev.eu-gb.mybluemix.net/v1/data?deviceId=" + deviceID + "&type=AIROWL&key=hetvi_1234&pm1=" + String(PM25) + "&pm25=" + String(PM10) + "&pm10=" + String(PM1) + "\"";
 
-  //Serial..println(command);
+  String command = "AT+HTTPPARA=\"URL\",\"http://oedpdev.eu-gb.mybluemix.net/v1/data?deviceId=" + deviceID + "&type=AIROWL&key=hetvi_1234&pm1=" + String(PM25) + "&pm25=" + String(PM10) + "&pm10=" + String(PM1) + "\"";
+
+  //Serial.println(command);
   GSM_Serial.println(command);
 
   delay(2000);
@@ -135,20 +122,20 @@ void SubmitHttpRequest()
 
   if (check(0, 5000))
   {
-    //Serial..println("Send ok");
+    //Serial.println("Send ok");
   }
   else
   {
-    //Serial..println("Send err");
+    //Serial.println("Send err");
   }
   //delay(1000);
   if (check(2, 10000))
   {
-    //Serial..println("Send ok");
+    //Serial.println("Send ok");
   }
   else
   {
-    //Serial..println("Send err");
+    //Serial.println("Send err");
   }
 
   rx_empty();
@@ -157,31 +144,31 @@ void SubmitHttpRequest()
 
   if (check(0, 10000))
   {
-    //Serial..println("GSM done");
+    //Serial.println("GSM done");
   }
   else
   {
-    //Serial..println("GSM err");
+    //Serial.println("GSM err");
   }
 
   GSM_Serial.println("AT+HTTPTERM");
   if (check(0, 5000))
   {
-    //Serial..println("Terminate done");
+    //Serial.println("Terminate done");
   }
   else
   {
-    //Serial..println("Terminate err");
+    //Serial.println("Terminate err");
   }
 
   GSM_Serial.println("AT+SAPBR=0,1");
   if (check(0, 5000))
   {
-    //Serial..println("Terminate done");
+    //Serial.println("Terminate done");
   }
   else
   {
-    //Serial..println("Terminate err");
+    //Serial.println("Terminate err");
   }
 
   digitalWrite(A0, LOW);
@@ -189,51 +176,78 @@ void SubmitHttpRequest()
   digitalWrite(A2, LOW);
 }
 
-void Winsen_dust()
+boolean Winsen_dust(unsigned int &PM1, unsigned int &PM25, unsigned int &PM10)
 {
 
-  Dust_Serial.listen();
+  PM1 = 0, PM25 = 0, PM10 = 0;
+  unsigned int count = 0;
 
-  int i = 0;
+  unsigned long lastPublishMillis = millis();
 
-  while (!Dust_Serial.available());
-  while (Dust_Serial.available())
+  byte data[24];
+  byte i = 0;
+  int sum;
+
+  while (1)
   {
-    data[i] = Dust_Serial.read();
+    i = 0;
+    Dust_Serial.listen();
 
-    if (i == 23)
+    while (!Dust_Serial.available());
+    while (Dust_Serial.available())
     {
-      Dust_Serial.println();
-      PM1 += ((data[4] * 256) + data[5]);
-      PM25 += ((data[6] * 256) + data[7]);
-      PM10 += ((data[8] * 256) + data[9]);
-      count++;
-      //Serial..print("PM 1.0 :");
-      //Serial..println(PM1 / count);
-      //Serial..print("PM 2.5 :");
-      //Serial..println(PM25 / count);
-      //Serial..print("PM 10 :");
-      //Serial..println(PM10 / count);
-      //Serial..println("");
+      data[i] = Dust_Serial.read();
+      if (data[0] == 0x42)
+      {
+        if (data[1] == 0x4D)
+        {
+          if (i == 23)
+          {
+            PM1 += ((data[4] * 256) + data[5]);
+            PM25 += ((data[6] * 256) + data[7]);
+            PM10 += ((data[8] * 256) + data[9]);
+            count++;
+            //Serial.print("count : ");
+            //Serial.println(count);
+            //Serial.print("PM 1.0 :");
+            //Serial.println(PM1 / count);
+            //Serial.print("PM 2.5 :");
+            //Serial.println(PM25 / count);
+            //Serial.print("PM 10 :");
+            //Serial.println(PM10 / count);
+            //Serial.println("");
+            Dust_Serial.flush();
+            break;
+          }
+        }
+        i++;
+        delay(10);
+      }
     }
-    i++;
-    delay(10) ;
+
+    delay(2000);
+    if (millis() - lastPublishMillis > 100000UL)
+    {
+      PM1 = PM1 / count;
+      PM25 = PM25 / count;
+      PM10 = PM10 / count;
+      return 1;
+    }
   }
-  //Serial..println("Calculation done");
 }
 
-void LED_blink()
+void LED_blink(unsigned int PM1, unsigned int PM25, unsigned int PM10)
 {
   //Ranges
   const int Range1_max = 120; const int Range1_min = 60;
   const int Range25_max = 350; const int Range25_min = 100;
   const int Range10_max = 1200; const int Range10_min = 951;
 
-  if ( PM1 > Range1_max || PM25 > Range25_max || PM10 > Range10_max )
+  if ( PM1 > Range1_max && PM25 > Range25_max && PM10 > Range10_max )
   {
     red();
   }
-  else if ( PM1 > Range1_min || PM25 > Range25_min || PM10 > Range10_min )
+  else if ( PM1 > Range1_min && PM25 > Range25_min && PM10 > Range10_min )
   {
     yellow();
   }
@@ -267,15 +281,14 @@ void red()
 void yellow()
 {
   analogWrite(A0, 0);
-  analogWrite(A1, 128);
+  analogWrite(A1, 1024);
   analogWrite(A2, 1024);
 }
 
-bool check(int v, uint32_t timeout)
+bool check(int v, uint32_t timeout, String &Data)
 {
   unsigned long start = millis();
   char a;
-  Data = "";
 
   while (millis() - start < timeout) {
     while (GSM_Serial.available() > 0) {
@@ -285,35 +298,70 @@ bool check(int v, uint32_t timeout)
     }
     if (Data.indexOf("OK") != -1 && v == 0)
     {
-      //Serial..println(Data);
+      //Serial.println(Data);
       return 1;
     }
     if (Data.indexOf(">") != -1 && v == 1)
     {
-      //Serial..println(Data);
+      //Serial.println(Data);
       return 1;
     }
     if (Data.indexOf(":") != -1 && v == 2)
     {
-      //Serial..println(Data);
+      //Serial.println(Data);
       return 1;
     }
   }
-  //Serial..println(Data);
+  //Serial.println(Data);
+  return 0;
+}
+
+bool check(int v, uint32_t timeout)
+{
+  unsigned long start = millis();
+  char a;
+  String Data;
+
+  while (millis() - start < timeout) {
+    while (GSM_Serial.available() > 0) {
+      a = GSM_Serial.read();
+      if (a == '\0') continue;
+      Data += a;
+    }
+    if (Data.indexOf("OK") != -1 && v == 0)
+    {
+      //Serial.println(Data);
+      return 1;
+    }
+    if (Data.indexOf(">") != -1 && v == 1)
+    {
+      //Serial.println(Data);
+      return 1;
+    }
+    if (Data.indexOf(":") != -1 && v == 2)
+    {
+      //Serial.println(Data);
+      return 1;
+    }
+  }
+  //Serial.println(Data);
   return 0;
 }
 
 void deviceId(void)
 {
+  String tmp_id;
+
   GSM_Serial.listen();
+
 
   GSM_Serial.println("AT+GSN");
 
-  if (check(0, 5000))
+  if (check(0, 5000, tmp_id))
   {
-    int first = Data.indexOf("\n");
-    int second = Data.indexOf("\n", first + 3);
-    deviceID = deviceID + Data.substring(first + 13, second - 1);
+    int first = tmp_id.indexOf("\n");
+    int second = tmp_id.indexOf("\n", first + 3);
+    deviceID = deviceID + tmp_id.substring(first + 13, second - 1);
   }
 }
 
